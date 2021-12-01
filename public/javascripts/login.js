a167f2a4e35e3a9685c19e25c1d161076800210a
MinJunKimsdaads <aaeexx@naver.com>
2021-12-01 23:34:54 +0900

    dass

---
+++ public/javascripts/login.js
@@ -0,0 +1,30 @@
+
+const id=document.getElementById('id');
+const password=document.getElementById('password');
+const submit=document.getElementById('submit');
+
+submit.addEventListener('click',login);
+// console.log(id)
+
+function login(){
+    const req={
+        id: id.value,
+        password: password.value,
+    }; 
+    fetch('/',{
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(req),
+    })
+      .then((res)=>res.json())
+      .then((res)=>{
+          if(res.success){
+              location.href='/resister';
+          } else {
+              alert(res.msg);
+          }
+      });
+}
+ 
--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,35 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+  res.render('login', { title: 'Express' });
+});
+
+const users = {
+  id: ['kmj'],
+  password: ["1234"],
+};
+
+router.post('/', function(req, res) {
+  const id =  req.body.id,
+   password = req.body.password;
+  
+  if (users.id.includes(id)) {
+    const idx = users.id.indexOf(id);
+    if(users.password[idx]===password) {
+      return res.json({
+        success: true,
+      });
+    }
+  }
+  return res.json({
+    success: false,
+    msg: '로그인 실패',
+  });
+})
+
+
+
+router.get('/resister', function(req, res, next) {
   res.render('resister', { title: 'Express' });
 });
 
