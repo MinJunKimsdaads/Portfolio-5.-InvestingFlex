ea547e02b6d6ba20d029e73875638e9435d0a52b
MinJunKimsdaads <aaeexx@naver.com>
2021-12-01 16:48:05 +0900

    123

---
+++ Invsesting/Investing/public/javascripts/resister.js
@@ -0,0 +1,21 @@
+
+const id=document.getElementById('id');
+const name=document.getElementById('name');
+const password=document.getElementById('password');
+const repeatpassword=document.getElementById('repeatpassword');
+const submit=document.getElementById('submit');
+
+submit.addEventListener('click',resister);
+// console.log(id)
+
+function resister(){
+    const req={
+        id: id.value,
+        name: name.value,
+        password: password.value,
+        repeatpassword: repeatpassword.value,
+    };
+
+    // fetch();
+}
+
